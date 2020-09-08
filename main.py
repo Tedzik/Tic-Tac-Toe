52c20cc81cd53fb30d8c6c5b8c3a5fb008e8f382
Tedzik <patryk.tede@gmail.com>
2020-09-08 17:06:53 +0100

    current player works properly, check if win for columns

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # ---- Global Variables ----
 
 # Game board
-board = ["-","-","-",
-         "-","-","-",
-         "-","-","-"]
-
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
 
 # If game is still going
 
@@ -18,6 +17,7 @@ current_player = "X"
 
 winner = None
 
+
 def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
     print(board[3] + " | " + board[4] + " | " + board[5])
@@ -25,65 +25,68 @@ def display_board():
 
 
 def play_game():
-
     # display initial board
     display_board()
     while game_still_going:
-
         handle_turn(current_player)
-
         check_if_game_over()
-
         flip_player()
 
-# the game has ended
-if winner == "X" or winner == "O":
-    print(winner + "Won.")
-elif winner == None:
-    print("Tie.")
+    # the game has ended
+    if winner == "X" or winner == "O":
+        print(winner + "Won.")
+    elif winner is None:
+        print("Tie.")
+
 
 def handle_turn(player):
     position = input("Choose position from 1-9: ")
     position = int(position) - 1
 
-    board[position] = "X"
+    board[position] = current_player
     display_board()
+
 
 def check_if_game_over():
     check_if_win()
     check_if_tie()
 
+
 def check_if_win():
+    global board, winner, current_player, game_still_going
+    if (board[0] == current_player and board[3] == current_player and board[6] == current_player) \
+            or (board[1] == current_player and board[4] == current_player and board[7] == current_player) \
+            or (board[2] == current_player and board[5] == current_player and board[8] == current_player):
+        winner = current_player
+        game_still_going = False
     # check columns
     # check rows
     # check diagonals
     return
 
+
 def check_if_tie():
     return
 
+
 def flip_player():
+    global current_player
+    if current_player == "X":
+        current_player = "O"
+    else:
+        current_player = "X"
     return
 
 
-
-
 play_game()
-
-
-
-
-
-
-
 
 # board
 # display board
 # play game
 # handle turn
 # check win
-    # check columns
-    # check rows
-    # check diagonals
+# check columns
+# check rows
+# check diagonals
 # check ties
 # flip players
