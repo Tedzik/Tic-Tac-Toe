e275fc79a0d47ed8b93b64ec651e5c610b356e83
Tedzik <patryk.tede@gmail.com>
2020-09-09 18:24:34 +0100

    init

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-# ---- Global Variables ----
 
-# Game board
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
 
-# If game is still going
 
 game_still_going = True
 
-# whos turn is it?
 
 current_player = "X"
 
-# who won? or tie?
 
 winner = None
 
@@ -25,23 +20,36 @@ def display_board():
 
 
 def play_game():
-    # display initial board
+
     display_board()
+
     while game_still_going:
         handle_turn(current_player)
         check_if_game_over()
         flip_player()
 
-    # the game has ended
     if winner == "X" or winner == "O":
-        print(winner + "Won.")
+        print(winner + " Won.")
     elif winner is None:
         print("Tie.")
 
+def handle_turn(current_player):
 
-def handle_turn(player):
-    position = input("Choose position from 1-9: ")
-    position = int(position) - 1
+    print(current_player + "'s turn.")
+    position = input("Choose a position from 1-9: ")
+
+    valid = False
+    while not valid:
+
+        while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            position = input("Choose a position from 1-9: ")
+
+        position = int(position) - 1
+
+        if board[position] == "-":
+            valid = True
+        else:
+            print("You can't go there. Go again.")
 
     board[position] = current_player
     display_board()
@@ -56,17 +64,27 @@ def check_if_win():
     global board, winner, current_player, game_still_going
     if (board[0] == current_player and board[3] == current_player and board[6] == current_player) \
             or (board[1] == current_player and board[4] == current_player and board[7] == current_player) \
-            or (board[2] == current_player and board[5] == current_player and board[8] == current_player):
+            or (board[2] == current_player and board[5] == current_player and board[8] == current_player) \
+            or (board[0] == current_player and board[1] == current_player and board[2] == current_player) \
+            or (board[3] == current_player and board[4] == current_player and board[5] == current_player) \
+            or (board[6] == current_player and board[7] == current_player and board[8] == current_player) \
+            or (board[0] == current_player and board[4] == current_player and board[8] == current_player) \
+            or (board[2] == current_player and board[4] == current_player and board[6] == current_player):
         winner = current_player
         game_still_going = False
-    # check columns
-    # check rows
-    # check diagonals
+
     return
 
 
 def check_if_tie():
-    return
+  global game_still_going
+
+  if "-" not in board:
+    game_still_going = False
+    return True
+
+  else:
+    return False
 
 
 def flip_player():
@@ -80,13 +98,4 @@ def flip_player():
 
 play_game()
 
-# board
-# display board
-# play game
-# handle turn
-# check win
-# check columns
-# check rows
-# check diagonals
-# check ties
-# flip players
+
